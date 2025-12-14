1ac172acef1a362cdd860590924325de00e05208
Jacopo Minniti <jacopo.minniti004@gmail.com>
2025-12-14 15:35:59 -0300

    changed name identifier

--- shinka/llm/query.py
+++ shinka/llm/query.py
@@ -194,6 +194,7 @@ def query(
     **kwargs,
 ) -> QueryResult:
     """Query the LLM."""
+    original_model_name = model_name
     client, model_name = get_client_llm(
         model_name, structured_output=output_model is not None
     )
@@ -205,7 +206,7 @@ def query(
         query_fn = query_deepseek
     elif model_name in GEMINI_MODELS.keys():
         query_fn = query_gemini
-    elif model_name.startswith("local-"):
+    elif original_model_name.startswith("local-"):
         query_fn = query_local
     else:
         raise ValueError(f"Model {model_name} not supported.")
